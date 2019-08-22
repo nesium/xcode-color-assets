b1db2a50f5fa643734f88d5ee8f8b619890d4697
mb <mb@nesium.com>
2019-08-22 11:51:02 +0700

    bumped version#

--- xcode-color-assets/src/main.rs
+++ xcode-color-assets/src/main.rs
@@ -7,7 +7,7 @@ use swift_gen::gen_swift;
 
 fn main() {
   let matches = App::new("xcode-color-assets")
-    .version("0.3.0")
+    .version("0.3.1")
     .about("Create Xcode Asset Catalog with colors for light & dark mode.")
     .subcommand(
       SubCommand::with_name("gen-assets")
