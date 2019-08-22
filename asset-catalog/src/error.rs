dc2ec7a1a28c71e11f8cf7a76ba53c996a8f9b55
mb <mb@nesium.com>
2019-08-22 10:57:59 +0700

    Rust 1.37.0 changes

--- asset-catalog/src/error.rs
+++ asset-catalog/src/error.rs
@@ -5,32 +5,34 @@ pub enum Error {
   CatalogExists(String),
   CouldNotCreateFile(String),
   CouldNotCreateDirectory(String),
-  CouldNotRemoveDirectory(String), 
+  CouldNotRemoveDirectory(String),
   UnknownIdentifier(String),
-  AssignColorSetToLightProperty(String), 
-  AssignColorSetToDarkProperty(String)
+  AssignColorSetToLightProperty(String),
+  AssignColorSetToDarkProperty(String),
 }
 
 impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     let message = match self {
-      Error::CatalogExists(path) => format!("Asset Catalog already exists at path {}.", path),
-      Error::CouldNotCreateFile(path) => format!("Could not create file at path {}.", path),
-      Error::CouldNotCreateDirectory(path) => {
+      Self::CatalogExists(path) => format!("Asset Catalog already exists at path {}.", path),
+      Self::CouldNotCreateFile(path) => format!("Could not create file at path {}.", path),
+      Self::CouldNotCreateDirectory(path) => {
         format!("Could not create directory at path {}.", path)
       }
-      Error::CouldNotRemoveDirectory(path) => {
+      Self::CouldNotRemoveDirectory(path) => {
         format!("Could not remove directory at path {}.", path)
-      }, 
-      Error::UnknownIdentifier(identifier) => {
-        format!("Could not find variable with identifier {}.", identifier)
-      },
-      Error::AssignColorSetToLightProperty(identifier) => {
-        format!("Attempt to assign a colorset to the light property of another colorset via variable {}.", identifier)
-      },
-      Error::AssignColorSetToDarkProperty(identifier) => {
-        format!("Attempt to assign a colorset to the dark property of another colorset via variable {}.", identifier)
       }
+      Self::UnknownIdentifier(identifier) => {
+        format!("Could not find variable with identifier {}.", identifier)
+      }
+      Self::AssignColorSetToLightProperty(identifier) => format!(
+        "Attempt to assign a colorset to the light property of another colorset via variable {}.",
+        identifier
+      ),
+      Self::AssignColorSetToDarkProperty(identifier) => format!(
+        "Attempt to assign a colorset to the dark property of another colorset via variable {}.",
+        identifier
+      ),
     };
     write!(f, "Error: {}", message)
   }
